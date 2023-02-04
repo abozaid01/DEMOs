54d64473769b132cfe4a8534432af87a28ff8a99
abozaid01 <mohamed.abozaid01@gmail.com>
2023-02-04 21:50:03 +0200

    Auth registering

--- Auth Demo/index.js
+++ Auth Demo/index.js
@@ -2,12 +2,43 @@ const express = require("express");
 const app = express();
 const User = require("./models/user");
 const path = require("path");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+mongoose
+    .connect("mongodb://localhost:27017/authDemo")
+    .then(() => {
+        console.log("Monge connection open ...");
+    })
+    .catch((err) => {
+        console.log("mongo connection error!!", err);
+    });
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.send("this is a Home Page!");
+});
+
 app.get("/register", (req, res) => {
     res.render("register");
+});
+
+app.post("/register", async (req, res) => {
+    const { username, password } = req.body;
+    const hash = await bcrypt.hash(password, 12);
+
+    const user = new User({
+        username,
+        password: hash,
+    });
+
+    await user.save();
+
+    res.redirect("/");
 });
 
 app.get("/secret", (req, res) => {
